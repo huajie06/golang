248579d546538e622064bec039ca95b90cd0b953
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-01-17 20:40:27 -0500

    add ignore and readme

---
+++ project1/ReadFile.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f := createFile("test.txt")
+	defer fileClose(f)
+	fileWrite(f)
+}
+
+func createFile(p string) *os.File {
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func fileClose(f *os.File) {
+	err := f.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func fileWrite(f *os.File) {
+	fmt.Fprintln(f, "bunch of data")
+}
