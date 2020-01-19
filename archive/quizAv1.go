147f6e330436ddd5658c32771a01465ecaa0143b
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-01-19 15:10:50 -0500

    quiz 1 different v

---
+++ archive/quizAv1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("//Users/huajiezhang/go/src/project1/data/problems.csv")
+	csvR := csv.NewReader(file)
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Game Start!!\n")
+
+	for i := 0; ; i++ {
+		s, err := csvR.Read() //readin one by one
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Printf("%v = ", s[0])
+		scanner.Scan()
+
+		ans := scanner.Text()
+		if s[1] != ans {
+			fmt.Printf("Game over! You completed %v\n", i)
+			break
+		}
+	}
+}
---
+++ project1/version0.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("//Users/huajiezhang/go/src/project1/data/problems.csv")
+	csvR := csv.NewReader(file)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	s, _ := csvR.ReadAll()
+
+	fmt.Printf("Game Start!!\n-----\n")
+	for k, v := range s {
+		fmt.Printf("Question #%v, %v = ", k+1, v[0])
+		scanner.Scan()
+		ans := scanner.Text()
+		if ans != v[1] {
+			fmt.Printf("Game over, you scored %v out of %v.\n", k, len(s))
+			break
+		}
+	}
+
+}
