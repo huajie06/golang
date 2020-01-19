7c86a9cb62332b84b9cc1dd583179c90c0f9a108
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-01-18 23:29:04 -0500

    adjust the for loop

--- test/test_scan.go
+++ test/test_scan.go
@@ -7,14 +7,14 @@ import (
 )
 
 // PROMPT used as flag
-const PROMPT = "..."
+const PROMPT = "go>> "
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		fmt.Printf(PROMPT)
-		fmt.Println(scanner.Text())
-		if scanner.Text() == "quit()" {
+	for fmt.Printf(PROMPT); scanner.Scan(); fmt.Printf(PROMPT) {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if ln == "quit()" {
 			return
 		}
 	}
