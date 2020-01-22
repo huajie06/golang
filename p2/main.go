6065a53faf6e30c8dc7a4024dd587081aa42d821
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-01-21 21:55:56 -0500

    add as a package

---
+++ p2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"p2/urlshort"
+)
+
+func main() {
+	fmt.Println(urlshort.Test("hello"))
+	fmt.Println("hello")
+}
---
+++ p2/urlshort/urlshort.go
@@ -0,0 +1,5 @@
+package urlshort
+
+func Test(s string) string {
+	return s
+}
