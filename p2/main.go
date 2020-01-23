2cd1b692cb14bf62a4a0dda244a7f5428ef3f442
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-01-22 22:32:25 -0500

    use handler for mux.Handle

--- p2/main.go
+++ p2/main.go
@@ -6,15 +6,22 @@ import (
 	// "p2/urlshort"
 )
 
+// S1 ...
+type S1 struct{}
+
+func (S1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "user handler not handle or handlefunc")
+}
+
 func main() {
 	// urlshort.Test()
-	// mux := defaultMux()
+	mux := defaultMux()
 	// mapHandler := urlshort.Test()
 
-	h := hello
-	http.HandleFunc("/hello", h)
+	mux.Handle("/api/", S1{})
+
 	fmt.Println("server starting on: 8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", mux)
 }
 
 func defaultMux() *http.ServeMux {
