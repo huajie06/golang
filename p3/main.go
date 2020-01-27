3457b9f879ce481f0bf5a728aed6e6d5919eb1de
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-01-26 20:43:16 -0500

    static site version

--- p3/main.go
+++ p3/main.go
@@ -3,106 +3,57 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"os"
-	"text/template"
 )
 
-type JsonData struct {
-	Intro struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"intro"`
-	NewYork struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"new-york"`
-	Debate struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"debate"`
-	SeanKelly struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"sean-kelly"`
-	MarkBates struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"mark-bates"`
-	Denver struct {
-		Title   string   `json:"title"`
-		Story   []string `json:"story"`
-		Options []struct {
-			Text string `json:"text"`
-			Arc  string `json:"arc"`
-		} `json:"options"`
-	} `json:"denver"`
-	Home struct {
-		Title   string        `json:"title"`
-		Story   []string      `json:"story"`
-		Options []interface{} `json:"options"`
-	} `json:"home"`
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+type Chapter struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
 }
 
-func parseJson(s string) *JsonData {
+func parseJson(s string) *map[string]Chapter {
 	f, err := ioutil.ReadFile(s)
 	if err != nil {
 		panic(err)
 	}
-	var dat = JsonData{}
-	if err := json.Unmarshal(f, &dat); err != nil {
+	var JsonData = make(map[string]Chapter)
+	if err := json.Unmarshal(f, &JsonData); err != nil {
 		panic(err)
 	}
-	return &dat
+	return &JsonData
 }
 
 func main() {
 	dat := parseJson("cyoa.json")
-	fmt.Println(dat.Intro.Title)
-	// fmt.Println(dat.Intro.Story)
-	// fmt.Println(dat.Intro.Options)
-	// for _, v := range dat.Intro.Story {
-	// 	fmt.Println(v)
-	// }
 
-	// for _, v := range dat.Intro.Options {
-	// 	fmt.Println(v.Text, v.Arc)
-	// }
-
-	tmpl, err := template.ParseFiles("t.html")
+	tmpl, err := template.ParseFiles("template.html")
 	if err != nil {
 		panic(err)
 	}
+	for k, v := range *dat {
+		fmt.Println(k)
+		createHTML(k, tmpl, v)
+	}
 
-	f, err := os.Create("home.html")
+	// tmpl.Execute(os.Stdout, dat.Intro)
+
+}
+
+func createHTML(fname string, tmpl *template.Template, dt Chapter) {
+	f, err := os.Create(fname + ".html")
 	if err != nil {
 		log.Print(err)
 		return
 	}
 	defer f.Close()
 
-	// tmpl.Execute(os.Stdout, dat.Intro)
-	tmpl.Execute(f, dat.Intro)
-
+	tmpl.Execute(f, dt)
 }
