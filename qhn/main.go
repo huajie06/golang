e6a973c76782120a7d82b005961520b4edbbb625
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-02-20 23:49:33 -0500

    draft version

--- qhn/main.go
+++ qhn/main.go
@@ -6,46 +6,98 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"text/template"
 )
 
-const apiBase string = "https://hacker-news.firebaseio.com/v0"
+const apiBase string = "https://hacker-news.firebaseio.com/v0/"
 
 type story struct {
-	By    string `json:"by"`
-	Id    int    `json:"id"`
-	Title string `json:"title"`
-	Tt    string `json:"type"`
-	Url   string `json:"url"`
-	//Kids  []int  `json:"kids"`
+	Source string
+	By     string `json:"by"`
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Tt     string `json:"type"`
+	Url    string `json:"url"`
+}
+
+type storyHandle struct {
+	tmpl *template.Template
+	s    []story
+}
+
+func (s storyHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := s.tmpl.Execute(w, s.s)
+
+	if err != nil {
+		http.Error(w, "page not found", http.StatusNotFound)
+	}
+
+}
+
+func (s storyHandle) debug() {
+	s.tmpl.Execute(os.Stdout, s.s)
 }
 
 func main() {
-	var err error
+	// ids := getTopStory(5)
+	// fmt.Println(ids)
 
-	url2 := "https://hacker-news.firebaseio.com/v0/topstories.json"
-	r, err := http.Get(url2)
-	if err != nil {
-		log.Println(err)
-	}
+	// var ids = []int{22378679, 22380380, 22378555, 22376794, 22380364, 22379969}
+	// ret := returnIds(ids)
+	// fmt.Println(ret)
 
-	defer r.Body.Close()
+	//dat := []story{{Url: "abc", Title: "yeah!!!"}, {Url: "abc", Title: "yeah!!!"}}
 
-	var ids []int
+	ids := getTopStory(20)
+	dat := returnIds(ids)
 
-	dec := json.NewDecoder(r.Body)
-	err = dec.Decode(&ids)
-	if err != nil {
-		log.Println(err)
-	}
+	tmpl := template.Must(template.ParseFiles("template.html"))
+	h := storyHandle{tmpl, dat}
 
-	for _, v := range ids {
-		fmt.Println(v)
-	}
+	m := http.NewServeMux()
+	m.Handle("/", h)
+	http.ListenAndServe(":8000", m)
+
+	// h.debug()
 }
 
-func getById() {
+func getTopStory(numOfItems int) []int {
+	var err error
 
-	url := "https://hacker-news.firebaseio.com/v0/item/22371629.json"
+	url := fmt.Sprintf("%stopstories.json", apiBase)
+	r, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var ids []int
+	b := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = b.Decode(&ids)
+	if err != nil {
+		log.Println(err)
+	}
+	return ids[:numOfItems]
+}
+
+func returnIds(ids []int) []story {
+	ret := []story{}
+	for _, v := range ids {
+		ret = append(ret, getById(v))
+	}
+	return ret
+}
+
+func getById(id int) story {
+	//url := apiBase + "item/" + strconv.Itoa(id) + ".json"
+	url := fmt.Sprintf("%sitem/%d.json", apiBase, id)
+
 	r, err := http.Get(url)
 	if err != nil {
 		log.Println(err)
@@ -64,6 +116,11 @@ func getById() {
 		log.Println(err)
 	}
 
-	fmt.Println(s1)
+	if len(strings.Split(s1.Url, "/")) >= 2 {
+		s1.Source = strings.Split(s1.Url, "/")[2]
+	}
 
+	return s1
+
+	//fmt.Println(s1)
 }
