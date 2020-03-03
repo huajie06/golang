da3f58ca6d7b3c1b760033d9b1de02fd8d6e72ba
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-03-02 22:16:34 -0500

    second version

--- image/main.go
+++ image/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -21,9 +26,102 @@ const (
 )
 
 func main() {
-	err := primitive("1.jpeg", 10, withOpt(ModePolygon))
-	if err != nil {
-		fmt.Println(err)
+	mux := http.NewServeMux()
+	mux.Handle("/", renderImage())
+
+	ec := make(chan error, 1)
+	mux.Handle("/upload", upload(ec))
+	mux.Handle("/display", display(ec))
+
+	fs := http.FileServer(http.Dir("./img/"))
+	mux.Handle("/img/", http.StripPrefix("/img", fs))
+
+	fmt.Println("server started on :8000")
+	log.Println(http.ListenAndServe(":8000", mux))
+}
+
+func renderImage() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		html := `<html><body>
+			<form action="/upload" method="post" enctype="multipart/form-data">
+				<input type="file" name="image">
+				<input id="numberOfShapes" name="numberOfShapes" type="text" value="10">
+				<button type="submit">Upload Image</button>
+			</form>
+			</body></html>`
+		fmt.Fprint(w, html)
+		return
+	}
+}
+
+func upload(e chan<- error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		f, header, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, "something went wrong", http.StatusBadRequest)
+			return
+		}
+		defer f.Close()
+
+		ns, err := strconv.Atoi(r.FormValue("numberOfShapes"))
+		if err != nil {
+			http.Error(w, "something went wrong", http.StatusBadRequest)
+			return
+		}
+
+		// fmt.Println(header)
+
+		filename := header.Filename
+		fmt.Println(filename)
+
+		err = os.Remove("./img/new.jpeg")
+
+		nf, err := os.Create("./img/old.jpeg")
+		if err != nil {
+			http.Error(w, "something went wrong", http.StatusInternalServerError)
+			return
+		}
+
+		_, err = io.Copy(nf, f)
+		if err != nil {
+			http.Error(w, "something went wrong", http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/display", http.StatusFound)
+		// it needs to be http.StatusFound, otherwise it would not redirect correctly
+
+		go func(e chan<- error) {
+			err = primitive("./img/old.jpeg", "./img/new.jpeg", ns, withOpt(1))
+			e <- err
+		}(e)
+	}
+}
+
+func display(e <-chan error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		select {
+		case x, ok := <-e:
+			if ok {
+				fmt.Printf("Value %v was read.\n", x)
+			} else {
+				fmt.Println("Channel closed!")
+			}
+		default:
+			fmt.Println("No value ready, moving on.")
+		}
+
+		html := `<html><body>
+<h2>input imgs </h2><br>
+<img src="/img/old.jpeg" style="height:30%">
+<br>
+<br>
+<h2>output imgs </h2><br>
+<img src="/img/new.jpeg" style="height:30%">
+</body></html>`
+		fmt.Fprint(w, html)
+		return
 	}
 }
 
@@ -35,8 +133,8 @@ func withOpt(mode Mode) func() []string {
 	}
 }
 
-func primitive(infile string, numShapes int, opts ...func() []string) error {
-	outfile := fmt.Sprintf("%s%s", "out_", infile)
+func primitive(infile, outfile string, numShapes int, opts ...func() []string) error {
+	// outfile := fmt.Sprintf("%s%s", "out_", infile)
 	var args []string
 	for _, opt := range opts {
 		args = append(args, opt()...)
